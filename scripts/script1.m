fc917279dbf745df7878935e3369eb940c295004
Your Name <deckerwill7@gmail.com>
2023-03-24 16:40:55 -0700

    initial commit

---
+++ scripts/script1.m
@@ -0,0 +1,43 @@
+% DevERP Simplified
+% Authors: Will Decker and Julie M. Schneider
+%% Welcome Message
+
+% Hello and welcome to DevERP Simplified. 
+% 
+% The aim of this project is to develop and dispense a standard pipeline 
+% for analyzing developmental EEG data for ERPs while concurrently providing an easily readible framework 
+% for researchers to conduct ERP and EEG analysis regardless of prior coding abilities.
+
+%% Overview
+
+% Step 1: Establish directories
+% Step 2: Establish subject list
+% Step 3: Choosing which subjects to run
+% Step 4: Establish filter parameters
+
+%% Step 1: Establish directories
+
+% option 1
+
+dev_rawdir 
+dev_workdir
+dev_txtdir
+dev_erpdir
+
+% option 2
+
+dev_makedirs % reccommended
+
+%% Step 2: Establish subject list
+
+dev_IDsubjects
+
+%% Step 3: Choosing which subjects to run
+
+dev_subs2run
+
+%% Step 4: Establish filter parameters
+
+dev_filterparams
+
+%%
