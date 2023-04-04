c1ee7106a237d1a055ce63fb4d82cef928cc9a62
Your Name <deckerwill7@gmail.com>
2023-04-04 14:24:50 -0500

    make use of get_subs2run

--- functions/dev_mara.m
+++ functions/dev_mara.m
@@ -19,26 +19,8 @@ function [params, EEG, com] = dev_mara(varargin)
     EEG = [];
     com = ' ';
 
-    % load in subject list
-    if evalin('base', 'exist("subjects", "var")')
-        subjects = evalin('base', 'subjects')
-    elseif evalin('base', '~exist("subjects", "var")')
-        error(['Please make sure your subject list is loaded.'])
-    end
-
-    % load in subject start
-    if evalin('base', 'exist("subject_start", "var")')
-        subject_start = evalin('base', 'subject_start')
-    elseif evalin('base', '~exist("subject_start", "var")')
-        error(['Please make sure your subject_start is loaded.'])
-    end
-    
-    % load in subject end
-    if evalin('base', 'exist("subject_end", "var")')
-        subject_end = evalin('base', 'subject_end')
-    elseif evalin('base', '~exist("subject_end", "var")')
-        error(['Please make sure your subject_end is loaded.'])
-    end
+    % access subject list, subject start and subject end
+    get_subs2run
     
     % initiate UI
     if nargin > 0 && strcmp(varargin{1}, 'UI')
