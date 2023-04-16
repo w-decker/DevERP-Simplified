6d6ef0054b2b3204437f3f48e47325e6a4ad1584
Your Name <deckerwill7@gmail.com>
2023-04-16 07:28:42 -0500

    initial commit

---
+++ functions/get_params.m
@@ -0,0 +1,20 @@
+%% Function: get_params
+% Author: Will Decker
+% Usage: estblishes params structure
+% Inputs:
+    % none
+
+   % See https://github.com/w-decker/DevERP-Simplified/tree/main/functions or type devHELP in the command window.
+
+%%
+
+function get_params(~)
+
+    % if params struc already exists
+    if evalin('base', 'exist("params", "struct")') == 1
+        params = evalin('base', 'params');
+    % if params struc does not exist
+    elseif evalin('base', 'exist("params", "struct")') == 0
+        params = struct()
+        assignin('base', 'params', params)
+    end
