a488530cda8f8ec84cfcd905e25798b2e4045484
Your Name <deckerwill7@gmail.com>
2023-04-19 17:59:27 -0500

    initial commit

---
+++ functions/dev_loadraw.m
@@ -0,0 +1,56 @@
+%% Function: dev_loadraw
+% Author: Will Decker
+% Usage: Importing and loading raw files of different formats (e.g., BrainVision, BioSemi, etc.)
+% Inputs:
+    %
+
+   % See https://github.com/w-decker/DevERP-Simplified/tree/main/functions or type devHELP in the command window.
+
+function dev_loadraw(varargin)
+
+    % load in subject to run
+    if evalin('base', 'exist("subjects", "var")') == 1
+        subjects = evalin('base', 'subjects');
+    else
+        error('Variable ''subjects'' not found in the workspace.');
+    end
+
+    % load raw directory
+    if evalin('base', 'exist("rawdir", "var")') == 1
+        rawdir = evalin('base', 'rawdir');
+    else
+        error('Variable ''rawdir'' not found in the workspace.');
+    end
+
+    % load subject start
+    if evalin('base', 'exist("subject_start", "var")') == 1
+        subject_start = evalin('base', 'subject_start');
+    else
+        error('Variable ''subject_start'' not found in the workspace.');
+    end
+    
+    % load subject end
+    if evalin('base', 'exist("subject_end", "var")') == 1
+        subject_end = evalin('base', 'subject_end');
+    else
+        error('Variable ''subject_end'' not found in the workspace.');
+    end
+
+
+    % load EEGLAB
+    [ALLEEG, EEG, CURRENTSET, ALLCOM] = eeglab;
+    eeglab('redraw');
+
+    % Load in BrainVision files
+    if nargin > 0 && strcmp(varargin{1}, 'bv')
+
+        for s = subject_start:subject_end
+            subject = subjects{s};
+           
+            % use EEGLAB to load
+            EEG = pop_loadbv (rawdir, [subject '.vhdr'], [], []);
+            [ALLEEG, EEG, CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',subject,'gui','off'); 
+            EEG = eeg_checkset( EEG );
+        end
+    end
+
